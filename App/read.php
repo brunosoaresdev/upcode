e2e6fd364674e58be5111112abec7fbf7ac62e5a
Talles Airan <airan.talles@gmail.com>
2018-11-26 11:30:55 -0200

    Update read.php
    Example class

--- App/read.php
+++ App/read.php
@@ -1,2 +1,18 @@
-UP Code Framework
-========================
+<?php
+namespace App;
+/**
+*
+* Sample class for autoloader
+* Read class
+* @author Upcode
+*/
+class Read
+{
+    private $first_name;
+    public function __construct($first_name)
+    {
+        $this->first_name = $first_name; 
+    }
+  
+}
+?>
