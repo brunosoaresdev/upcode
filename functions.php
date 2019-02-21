b3febfd9030d11357b3e7ca51b2bfc4526af7ca7
Talles <airan.talles@gmail.com>
2019-02-21 15:44:54 -0300

    ACF

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
-
+use AcfUp;
 require_once ('vendor/autoload.php'); // uncomment this line if you want to use composer
+
 // Defines funcionts of the framework
 require_once ('functions/_setup.php');
 require_once ('functions/_custom.php');
@@ -10,8 +11,10 @@ require_once ('functions/_navwalker.php');
 require_once ('functions/_admin.php');
 
 // Defines custom functions
-require_once ('functions/_acf.php');
 require_once ('functions/_options.php');
 
 // Register custom post type
 // require_once ('cpt/cpt-product.php');
+if(class_exists('ACF')){
+    $acf = new AcfUp();    
+}
